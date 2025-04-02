bc7d64d148d9932d9e039ef30f985a3f768ac4ff
jasonotu <56510975+jasonotu@users.noreply.github.com>
2025-04-02 11:18:44 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Subdomain Takeover PoC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Subdomain Takeover PoC" />
         <p className="description">
-          Subdomain takeover PoC
+          416a6beb63de27d427effb71f3688808
         </p>
       </main>
 
